b628d6ae25c68478ae0333cc1e6d4bf33a477a9a
Marlon Axel <marlon.axel@tcsindustrial.com.br>
2022-08-02 19:28:58 -0300

    primeiro commit

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+/*Variável contendo a URL da api do gitHub, e os tipos de arquivos que serão usados na plataforma Take*/
 const api = {
     baseUrl:'https://api.github.com/users',
     itemType: 'application/vnd.lime.document-select+json',
@@ -11,28 +12,28 @@ const options = [];
 
 module.exports = {
 
+    //Função principal para buscar as informações dentro da API
     async getInfoGitHub(request, response) {
 
         //Limpa o array para não acumular dados repetidos a cada requisição
         items.length > 0  ? items.length = 0 :items
         
-
         const {name} = request.params
 
+        //Requisição axios na API do gitHub passando o user como paramêtro da busca
         await axios.get(`${api.baseUrl}/${name}/repos`).then(res=>{
             const repos = res.data
             console.log(`${api.baseUrl}/${name}/repos`);
-            repos.map(el =>{
+            repos.map(el =>{ // Percorre o objeto encontrado para filtrar e selecionar os dados que serão consumidos
                 const image = el.owner.avatar_url
                 const titleCard = el.name
                 const subTitleCard = el.description
                 const dateRepository = el.created_at
 
-
-
+                //If(){} para pegar somente por uma linguagem 
                 if(el.language == 'C#'){
                     
-                    items.push({
+                    items.push({ // Adiciona os dados ao array
                         header:{
                             type:api.type,
                             value:{
@@ -44,17 +45,16 @@ module.exports = {
                             }
                            },options})
 
-                    items.sort((a,b)=>{
+                    items.sort((a,b)=>{ // ordena as datas pelo de forma crescente
                         let aux1 = new Date(a.header.value.date)
                         let aux2 = new Date(b.header.value.date)
                         return aux1 - aux2
                     });
                 }
             })
-            console.log({itemType: api.itemType,items});
+            //Retorna o array em json
             return response.json({itemType: api.itemType,items})
             
-            console.log({itemType: api.itemType,items});
         }).catch(err=>{
             console.log('Ocorreu algum erro na requisição:: ',err);
         })
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
+//Importações
 const  express = require("express");
 const router = express.Router()
 const controller = require('../controller/controller')
 
+//Rota para buscar os dados do gitHub
 router.get('/buscaDados/:name', controller.getInfoGitHub)
 
-
+//Exportando as rotas para ser utilizado em outras partes do código
 module.exports = router
